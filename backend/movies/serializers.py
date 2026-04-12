2f2f8eafe55f38243fa15630b8952cfeb410851b
Emma k <146366765+Alpha-ema@users.noreply.github.com>
2026-04-12 12:57:44 +0300

    added default and optional handling for genre_ids in TMDB serializer

--- backend/movies/serializers.py
+++ backend/movies/serializers.py
@@ -124,7 +124,7 @@ class TMDBMovieSerializer(serializers.Serializer):
     popularity = serializers.FloatField()
     poster_path = serializers.CharField(allow_blank=True, allow_null=True)
     backdrop_path = serializers.CharField(allow_blank=True, allow_null=True)
-    genre_ids = serializers.ListField(child=serializers.IntegerField())
+    genre_ids = serializers.ListField(child=serializers.IntegerField(), required=False, default=[])
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
